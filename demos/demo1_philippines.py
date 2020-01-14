9423fa6c280d0756589ce58fedd8f2794c210ad1
Christian Werner <chr.werner@me.com>
2020-01-14 22:32:23 +0100

    first try to enable cartopy plotting code

--- demos/demo1_philippines.py
+++ demos/demo1_philippines.py
@@ -110,16 +110,15 @@ def plot_maps(management='Conventional', year=None):
     import matplotlib
     matplotlib.rcParams.update({'font.size': 10})
     import matplotlib.patches as mpatches
-    #import cartopy.crs as ccrs
-    #import cartopy.feature as cfeature
+    import cartopy.feature as cfeature
     #import cartopy.io.shapereader as shapereader    
 
     # Create a feature for States/Admin 1 regions at 1:50m from Natural Earth
-    #countries = cfeature.NaturalEarthFeature(
-    #    category='cultural',
-    #    name='admin_0_countries',
-    #    scale='50m',
-    #    facecolor='none')
+    countries = cfeature.NaturalEarthFeature(
+        category='cultural',
+        name='admin_0_countries',
+        scale='50m',
+        facecolor='none')
 
     #countries2 = shapereader.natural_earth(resolution='50m',
     #                                    category='cultural',
@@ -132,12 +131,12 @@ def plot_maps(management='Conventional', year=None):
     #else:
     #    raise ValueError('Unable to find the PHL boundary.')
         
-    #ocean = cfeature.NaturalEarthFeature(
-    #        category='physical', 
-    #        name='ocean', 
-    #        scale='50m',
-    #        edgecolor='face',
-    #        facecolor=cfeature.COLORS['water'])
+    ocean = cfeature.NaturalEarthFeature(
+            category='physical', 
+            name='ocean', 
+            scale='50m',
+            edgecolor='face',
+            facecolor=cfeature.COLORS['water'])
 
     Dname = {}
     Dname['dC_ch4_emis'] = r'$\mathregular{CH_4\ emission\ [kg\ C\ ha^{-1}\ yr^{-1}]}$'
@@ -157,35 +156,35 @@ def plot_maps(management='Conventional', year=None):
     da_n2o   = davg['dN_n2o_emis']
     da_gwp   = convert_ch4_gwp(da_ch4) + convert_n2o_gwp(da_n2o)
 
-    fig, axes = plt.subplots(2, 2, figsize=(10,10)) #, subplot_kw=dict(projection=ccrs.PlateCarree()))
+    fig, axes = plt.subplots(2, 2, figsize=(10,10), subplot_kw=dict(projection=ccrs.PlateCarree()))
 
     for i, ax in enumerate(axes.flat):
         if i == 3:
             break
-        #ax.set_extent([116, 127, 4, 22])
+        ax.set_extent([116, 127, 4, 22])
             
         # add map elements
-        #ax.add_feature(countries, edgecolor='gray')     # country borders
+        ax.add_feature(countries, edgecolor='gray')     # country borders
         #ax.add_geometries(vietnam, ccrs.PlateCarree(), edgecolor='white', facecolor='none')
-        #ax.add_feature(ocean)                           # ocean
-        #ax.coastlines(resolution='50m')                 # coastlines
+        ax.add_feature(ocean)                           # ocean
+        ax.coastlines(resolution='50m')                 # coastlines
 
         if i == 0:
             colors = plt.cm.Reds
             p=xr.plot.pcolormesh(da_ch4.where(da_ch4>0), cbar_kwargs={'label': Dname['dC_ch4_emis']}, 
-                        ax=ax, cmap=colors, #transform=ccrs.PlateCarree(), 
+                        ax=ax, cmap=colors, transform=ccrs.PlateCarree(), 
                         vmin=0, vmax=1500, extend='max' )
 
         elif i == 1:
             colors = plt.cm.YlGn
             xr.plot.pcolormesh(da_n2o.where(da_n2o>0), cbar_kwargs={'label': Dname['dN_n2o_emis']},
-                        ax=ax, cmap=colors, #transform=ccrs.PlateCarree(), 
+                        ax=ax, cmap=colors, transform=ccrs.PlateCarree(), 
                         vmin=0, vmax=5, extend='max' )
 
         elif i == 2:
             colors = plt.cm.Blues
             xr.plot.pcolormesh(da_gwp.where(da_gwp>0), cbar_kwargs={'label': Dname['GWP']}, 
-                        ax=ax, cmap=colors, #transform=ccrs.PlateCarree(), 
+                        ax=ax, cmap=colors, transform=ccrs.PlateCarree(), 
                         vmin=0, vmax=50, extend='max')
 
         #sublabels='ABC'
@@ -198,8 +197,8 @@ def plot_maps(management='Conventional', year=None):
         sublabels = ['CH4', 'N2O', 'CH4+N2O']
 
         ax.text(116.5, 20.45, sublabels[i], fontsize=15,
-                horizontalalignment='left', verticalalignment='center') 
-                #transform=ccrs.PlateCarree())
+                horizontalalignment='left', verticalalignment='center',
+                transform=ccrs.PlateCarree())
 
     fig.delaxes(axes[1][1])
 
